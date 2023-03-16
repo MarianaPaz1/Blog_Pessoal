d86a2a363b308dce138547efaef0df96aaa92e66
Mariana <mnp1807@hotmail.com>
2023-03-16 15:03:06 -0300

    atividade 03

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -29,6 +30,8 @@ import jakarta.validation.Valid;
 public class PostagemController {
 	@Autowired
 	private PostagemRepository postagemRepository;
+	@Autowired
+	private TemaRepository temaRepository;
 
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll() {
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,64 +4,80 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 	
-	@NotBlank
-	@Size(min=3, max=100)
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String titulo;
 	
-	@NotNull
-	@Size(min=10, max=1000)
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
 
-	public Long getId() {
-		return id;
-	}
+    @ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getTexto() {
-		return texto;
-	}
+    public String getTitulo() {
+        return this.titulo;
+    }
 
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public LocalDateTime getData() {
-		return data;
-	}
+    public String getTexto() {
+        return this.texto;
+    }
 
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-	
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Tema getTema() {
+        return this.tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+
 }
